1d7221259a75403630833ecceafa18edf5b93f52
Rosario Pulella <RosarioPulella@gmail.com>
2021-03-18 00:38:41 -0400

    Use xdg to setup firefox as default browser.

--- xdg.nix
+++ xdg.nix
@@ -1,7 +1,26 @@
+let
+  browser = "firefox.desktop";
+in
 {
   xdg = {
     enable = true;
 
     userDirs.enable = true;
+
+    mime.enable = true;
+
+    mimeApps = {
+      enable = true;
+
+      defaultApplications = {
+        "x-scheme-handler/msteams"   = [ "teams.desktop"         ];
+        "x-scheme-handler/bitwarden" = [ "Bitwarden.desktop"     ];
+        "text/html"                  = [ browser ];
+        "x-scheme-handler/http"      = [ browser ];
+        "x-scheme-handler/https"     = [ browser ];
+        "x-scheme-handler/about"     = [ browser ];
+        "x-scheme-handler/unknown"   = [ browser ];
+      };
+    };
   };
 }
