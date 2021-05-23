adc338e646da6c36fa835087ce35d92baac3d0e7
Rosario Pulella <RosarioPulella@gmail.com>
2021-05-23 12:56:50 -0400

    Consoliate package and programs

--- home.nix
+++ home.nix
@@ -8,13 +8,12 @@ in
     ./vim
     ./gpg.nix
     ./xdg.nix
-    ./programs.nix
-    ./packages.nix
     # ./sway.nix
     ./kakoune.nix
     ./plasma.nix
     ./archive.nix
     ./yubikey.nix
+    ./firefox/firefox.nix
   ];
 
   home = {
@@ -28,5 +27,75 @@ in
     };
 
     language.base = "en_US";
+  };
+
+  home.packages = with pkgs; [
+    nix
+    gitAndTools.gh
+    gitAndTools.hub
+
+    alacritty
+
+    watchman
+    inotify-tools
+
+    hunspell
+    hunspellDicts.en-us
+    teams
+    okular
+
+    zoom-us
+
+    rnix-lsp
+
+    # needed so that coc can work https://github.com/neoclide/coc.nvim/wiki/Install-coc.nvim#requirements
+    nodejs
+
+    spotify
+    bitwarden
+    inkscape
+    libreoffice
+    exercism
+
+    skype
+    discord
+    signal-desktop
+    gitter
+
+    radicle-upstream
+  ];
+
+  programs = {
+    bash.enable = true;
+    bat.enable = true;
+    ssh.enable = true;
+
+    vscode = {
+      enable = true;
+      package = pkgs.vscodium;
+    };
+
+    git = {
+      enable = true;
+      userName = "Rosario Pulella";
+      userEmail = "RosarioPulella@gmail.com";
+
+      extraConfig = {
+        # core.editor = editor;
+        diff.tool = "vimdiff";
+        difftool.prompt = false;
+
+        mergetool.prompt = false;
+        merge = {
+          tool = "vimdiff";
+          conflictstyle = "diff3";
+        };
+      };
+    };
+
+    chromium = {
+      enable = true;
+      package = pkgs.brave;
+    };
   };
 }
