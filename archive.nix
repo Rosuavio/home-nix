2f0d1b4f71b53616d70888f13a1d36a1156825f1
Rosario Pulella <RosarioPulella@gmail.com>
2021-05-24 00:15:23 -0400

    Add a bunch of archiving and compression tools

--- archive.nix
+++ archive.nix
@@ -1,8 +1,28 @@
 {pkgs, ... }:
 {
   home.packages = with pkgs; [
+    # archive
+    gnutar
+    libarchive
+    cpio
+    
+    # compression
+    bzip2
+    gzip
+    lrzip
+    lz4
+    lzip
+    lzop
+    xz
+    zstd
+    
+    # archive and compression
+    p7zip
+    unrar
     unzip
+    zip
 
+    # gui tools
     ark
   ];
 }
