8d8b7487d6ee1119a0d50c09e81f434d66862bbf
Rosario Pulella <RosarioPulella@gmail.com>
2021-05-26 16:51:28 -0400

    Remove workaround for missing ff profile folder bug

--- firefox/default.nix
+++ firefox/default.nix
@@ -21,11 +21,6 @@ in
     profiles.Rosario = {
       id = 0;
       isDefault = true;
-
-      # WORKAROUND: This forces the profile folder to be created fix in https://github.com/nix-community/home-manager/pull/2035
-      settings = {
-          "browser.startup.homepage"= "https://nixos.org";
-      };
     };
 
     # TODO: Map all attrs to a list.
