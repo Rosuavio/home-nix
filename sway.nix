b855a07c1a94c5241701c48f40645dce199b140a
Rosario Pulella <RosarioPulella@gmail.com>
2021-07-10 00:27:22 -0400

    Simplfy sway config with mkOptionDefault

--- sway.nix
+++ sway.nix
@@ -24,83 +24,22 @@ in
         };
       };
 
-      keybindings = {
-        "${cfg.config.modifier}+Return" = "exec ${cfg.config.terminal}";
-        "${cfg.config.modifier}+Shift+q" = "kill";
-        "${cfg.config.modifier}+d" = "exec ${cfg.config.menu}";
+      keybindings =
+        let
+          modifier = config.wayland.windowManager.sway.config.modifier;
+        in pkgs.lib.mkOptionDefault {
+          "${modifier}+ctrl+l" = "exec ${swaylock}";
 
-        "${cfg.config.modifier}+${cfg.config.left}" = "focus left";
-        "${cfg.config.modifier}+${cfg.config.down}" = "focus down";
-        "${cfg.config.modifier}+${cfg.config.up}" = "focus up";
-        "${cfg.config.modifier}+${cfg.config.right}" = "focus right";
+          XF86MonBrightnessDown = "exec ${bctl} -q set 5%- && ${light} -G | cut -d'.' -f1 > $SWAYSOCK.wob";
+          XF86MonBrightnessUp   = "exec ${bctl} -q set +5% && ${light} -G | cut -d'.' -f1 > $SWAYSOCK.wob";
 
-        "${cfg.config.modifier}+Left" = "focus left";
-        "${cfg.config.modifier}+Down" = "focus down";
-        "${cfg.config.modifier}+Up" = "focus up";
-        "${cfg.config.modifier}+Right" = "focus right";
-
-        "${cfg.config.modifier}+Shift+${cfg.config.left}" = "move left";
-        "${cfg.config.modifier}+Shift+${cfg.config.down}" = "move down";
-        "${cfg.config.modifier}+Shift+${cfg.config.up}" = "move up";
-        "${cfg.config.modifier}+Shift+${cfg.config.right}" = "move right";
-
-        "${cfg.config.modifier}+Shift+Left" = "move left";
-        "${cfg.config.modifier}+Shift+Down" = "move down";
-        "${cfg.config.modifier}+Shift+Up" = "move up";
-        "${cfg.config.modifier}+Shift+Right" = "move right";
-
-        "${cfg.config.modifier}+b" = "splith";
-        "${cfg.config.modifier}+v" = "splitv";
-        "${cfg.config.modifier}+f" = "fullscreen toggle";
-        "${cfg.config.modifier}+a" = "focus parent";
-
-        "${cfg.config.modifier}+s" = "layout stacking";
-        "${cfg.config.modifier}+w" = "layout tabbed";
-        "${cfg.config.modifier}+e" = "layout toggle split";
-
-        "${cfg.config.modifier}+Shift+space" = "floating toggle";
-        "${cfg.config.modifier}+space" = "focus mode_toggle";
-
-        "${cfg.config.modifier}+1" = "workspace number 1";
-        "${cfg.config.modifier}+2" = "workspace number 2";
-        "${cfg.config.modifier}+3" = "workspace number 3";
-        "${cfg.config.modifier}+4" = "workspace number 4";
-        "${cfg.config.modifier}+5" = "workspace number 5";
-        "${cfg.config.modifier}+6" = "workspace number 6";
-        "${cfg.config.modifier}+7" = "workspace number 7";
-        "${cfg.config.modifier}+8" = "workspace number 8";
-        "${cfg.config.modifier}+9" = "workspace number 9";
-
-        "${cfg.config.modifier}+Shift+1" = "move container to workspace number 1";
-        "${cfg.config.modifier}+Shift+2" = "move container to workspace number 2";
-        "${cfg.config.modifier}+Shift+3" = "move container to workspace number 3";
-        "${cfg.config.modifier}+Shift+4" = "move container to workspace number 4";
-        "${cfg.config.modifier}+Shift+5" = "move container to workspace number 5";
-        "${cfg.config.modifier}+Shift+6" = "move container to workspace number 6";
-        "${cfg.config.modifier}+Shift+7" = "move container to workspace number 7";
-        "${cfg.config.modifier}+Shift+8" = "move container to workspace number 8";
-        "${cfg.config.modifier}+Shift+9" = "move container to workspace number 9";
-
-        "${cfg.config.modifier}+Shift+minus" = "move scratchpad";
-        "${cfg.config.modifier}+minus" = "scratchpad show";
-
-        "${cfg.config.modifier}+Shift+c" = "reload";
-        "${cfg.config.modifier}+Shift+e" =
-        "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
-
-        "${cfg.config.modifier}+r" = "mode resize";
-        "${cfg.config.modifier}+ctrl+l" = "exec ${swaylock}";
-
-        XF86MonBrightnessDown = "exec ${bctl} -q set 5%- && ${light} -G | cut -d'.' -f1 > $SWAYSOCK.wob";
-        XF86MonBrightnessUp   = "exec ${bctl} -q set +5% && ${light} -G | cut -d'.' -f1 > $SWAYSOCK.wob";
-
-        # PulseAudio needs to be on the system, maybe check first.
-        XF86AudioRaiseVolume = "exec ${pamixer} -ui 2 && ${pamixer} --get-volume > $SWAYSOCK.wob";
-        XF86AudioLowerVolume = "exec ${pamixer} -ud 2 && ${pamixer} --get-volume > $SWAYSOCK.wob";
-        XF86AudioMute = "exec ${pamixer} --toggle-mute && ( ${pamixer} --get-mute && echo 0 > $SWAYSOCK.wob ) || ${pamixer} --get-volume > $SWAYSOCK.wob";
-        # If no pactl then fallback to alsa
-        XF86AudioMicMute = "exec ${pactl} set-source-mute 1 toggle";
-      };
+          # PulseAudio needs to be on the system, maybe check first.
+          XF86AudioRaiseVolume = "exec ${pamixer} -ui 2 && ${pamixer} --get-volume > $SWAYSOCK.wob";
+          XF86AudioLowerVolume = "exec ${pamixer} -ud 2 && ${pamixer} --get-volume > $SWAYSOCK.wob";
+          XF86AudioMute = "exec ${pamixer} --toggle-mute && ( ${pamixer} --get-mute && echo 0 > $SWAYSOCK.wob ) || ${pamixer} --get-volume > $SWAYSOCK.wob";
+          # If no pactl then fallback to alsa
+          XF86AudioMicMute = "exec ${pactl} set-source-mute 1 toggle";
+        };
       ## Might want to find a new menu
       #menu = "${pkgs.rofi}/bin/rofi";
       # modifier = "";
