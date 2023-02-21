f6aa4e506d4fe3d67327f28cc0c841791da5c52b
Rosario Pulella <RosarioPulella@gmail.com>
2023-02-21 00:44:29 -0500

    Remove unneeded set sway session commands

--- graphical/sway.nix
+++ graphical/sway.nix
@@ -108,9 +108,6 @@ in
       export _JAVA_AWT_WM_NONREPARENTING=1
 
       export CLUTTER_BACKEND=wayland
-
-      export XDG_CURRENT_DESKTOP="sway"
-      export XDG_SESSION_TYPE="wayland"
     '';
   };
 }
