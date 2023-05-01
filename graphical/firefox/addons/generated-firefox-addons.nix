01dc29ce1067dc8eaacaba925654728123d60b32
Rosario Pulella <RosarioPulella@gmail.com>
2023-04-30 20:26:51 -0400

    Update deps

--- graphical/firefox/addons/generated-firefox-addons.nix
+++ graphical/firefox/addons/generated-firefox-addons.nix
@@ -2,10 +2,10 @@
   {
     "bitwarden" = buildFirefoxXpiAddon {
       pname = "bitwarden";
-      version = "2023.1.0";
+      version = "2023.3.1";
       addonId = "{446900e4-71c2-419f-a6a7-df9c091e268b}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/4054938/bitwarden_password_manager-2023.1.0.xpi";
-      sha256 = "b107930fdd005aac6946222ab8b80c789f17bd8e6ccd69c4f4a1cfe102b1c964";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4093799/bitwarden_password_manager-2023.3.1.xpi";
+      sha256 = "d4ea270874c013daf0aa8b46686766481464db0b3eaa271dc1a2e579751f36f7";
       meta = with lib;
       {
         homepage = "https://bitwarden.com";
@@ -16,10 +16,10 @@
       };
     "noscript" = buildFirefoxXpiAddon {
       pname = "noscript";
-      version = "11.4.16";
+      version = "11.4.21";
       addonId = "{73a6fe31-595d-460b-a920-fcc0f8843232}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/4067473/noscript-11.4.16.xpi";
-      sha256 = "44bdddd89bee11e52e09ea967aebd3aa996dc2d66c1a819e8dfdaf9a16cc753b";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4090970/noscript-11.4.21.xpi";
+      sha256 = "0fd3b66a2780d03a5b3cd460216105f3df2b27c6d3a552c1769c5de48c9e2338";
       meta = with lib;
       {
         homepage = "https://noscript.net";
@@ -30,10 +30,10 @@
       };
     "stylus" = buildFirefoxXpiAddon {
       pname = "stylus";
-      version = "1.5.26";
+      version = "1.5.30";
       addonId = "{7a7a4a92-a2a0-41d1-9fd7-1e92480d612d}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3995806/styl_us-1.5.26.xpi";
-      sha256 = "b30b14e9c4fa0c8d490d57e6b7d8afe6cc71e2f459b974b5c6fa2bfa32210294";
+      url = "https://addons.mozilla.org/firefox/downloads/file/4075896/styl_us-1.5.30.xpi";
+      sha256 = "9a8b40ecfb4e19252ab5435c3cb5976d9c86a7c295e4eda8a64820838b56b9ea";
       meta = with lib;
       {
         homepage = "https://add0n.com/stylus.html";
