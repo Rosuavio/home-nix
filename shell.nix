8ceae4942b610058c86c53200fd72cc3ad78ca18
Rosario Pulella <RosarioPulella@gmail.com>
2023-06-05 17:56:54 -0400

    Simplify dev-shell and use nixpkgs-unstable
    * The dev shell was providing more tools than needed for it, like
    home-manager.
    * The dev shel was setting up the enviorment for using home-manager
    directly on this repo. That is not how this repo is meant to be used.
    * Was using nixos-22.11 when as only a dev shell it might as well track
    latest nixpkgs-unstable.

--- shell.nix
+++ shell.nix
@@ -1,27 +1,19 @@
 let
 
   sources = import ./nix/sources.nix;
-
-  nixpkgs = sources."nixos-22.11";
-
-  pkgs = import nixpkgs {};
-
-  foo = pkgs.writeShellScriptBin "foo" "";
+  pkgs = import sources.nixpkgs-unstable {};
+  nur = import sources.nur-combined { inherit pkgs; };
 
 in pkgs.mkShell rec {
+  name = "home-dev";
 
-  name = "home-manager-shell";
-
-  buildInputs = with pkgs; [
-    niv
-    (import sources.home-manager {inherit pkgs;}).home-manager
-    (import sources.nur-combined {inherit pkgs;}).repos.ethancedwards8.firefox-addons-generator
-    foo
+  packages = [
+    pkgs.git
+    pkgs.niv
+    nur.repos.ethancedwards8.firefox-addons-generator
   ];
 
   shellHook = ''
-    export NIX_PATH="nixpkgs=${nixpkgs}:home-manager=${sources."home-manager"}"
-    export HOME_MANAGER_CONFIG="./home.nix"
+    echo 'Welcome to the home development shell!'
   '';
-
 }
