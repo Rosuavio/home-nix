3b24b93a45bfc40e6795efa6ee8d63199e100704
Rosario Pulella <RosarioPulella@gmail.com>
2024-01-04 17:53:47 -0500

    Some wayland sutff

--- graphical/sway.nix
+++ graphical/sway.nix
@@ -98,9 +98,15 @@ in
 
     extraSessionCommands = ''
       export SDL_VIDEODRIVER=wayland
+
+      # Xwayland compatibility
+      export XWAYLAND_NO_GLAMOR=1
+
+      export XDG_SESSION_TYPE=wayland
       # needs qt5.qtwayland in systemPackages
       export QT_QPA_PLATFORM=wayland
       export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+
       # Fix for some Java AWT applications (e.g. Android Studio),
       # use this if they aren't displayed properly:
       export _JAVA_AWT_WM_NONREPARENTING=1
